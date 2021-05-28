61eff68a2c47d7cb64591e0d72bd210957184a1f
prudhvi4399 <prudhvi88801@gmail.com>
2021-05-28 14:48:58 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,9 @@
-import First from './components/First'
-import Greet from './components/Greet';
-import Second from './components/Second'
-import Welcome from './components/Welcome'
+import Demo10 from "./components/Demo10";
+
 function App() {
   return (
     <div className="container">
-    
-        <h2 className='text-danger'>Welcome To React By RAJ</h2>
-        {/* <First></First>
-        <Second></Second> */}
-        {/* <Greet eid='1' ecity='Bangalore' ename='Raj'/>
-        <Greet eid='2' ecity='Goa' ename='Prudhvi'/>
-        <Greet eid='3' ecity='Hyderabad' ename='Rajeev'/> */}
-        <Welcome name='Rajeev' city='Goa'/>
-        <Welcome name='Prudhvi' city='Hyderabad'/>
+      <Demo10 />
     </div>
   );
 }
